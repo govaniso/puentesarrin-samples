cf5d5b64d7828c9157acfe79df84b2a32c5c4d3b
puentesarrin@gmail.com <puentesarrin@gmail.com@53c182f2-14e1-a3c1-e592-f3c75dab8e0f>
2011-11-06 00:29:55 +0000

    Initial commit

---
+++ sample_wssf/AppHost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace sample_wssf.AppHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost svEmployee = new ServiceHost(typeof(sample_wssf.ServiceImplementation.ServiceEmployee));
+            svEmployee.Open();
+            Console.WriteLine("Press any key to stop service.");
+            Console.ReadKey();
+            svEmployee.Close();
+        }
+    }
+}
---
+++ sample_wssf/sample_wssf/Source/Resource Access/sample_wssf.DataAccess/DAEmployee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using sample_wssf.DataContracts;
+
+namespace sample_wssf.DataAccess
+{
+    public class DAEmployee
+    {
+        public ListDCEmployee SelectAllEmployees()
+        {
+            try
+            {
+                ListDCEmployee Lista = new ListDCEmployee();
+                using (SqlConnection Connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Northwind"].ConnectionString))
+                {
+                    if (Connection.State == ConnectionState.Closed) Connection.Open();
+                    using (SqlCommand Command = new SqlCommand("select employeeid, lastname, firstname from employees", Connection))
+                    {
+                        using (IDataReader reader = Command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Lista.Add(new DCEmployee()
+                                {
+                                    EmployeeID = Convert.ToInt32(reader["employeeid"]),
+                                    FirstName = Convert.ToString(reader["firstname"]),
+                                    LastName = Convert.ToString(reader["lastname"])
+                                });
+                            }
+                        }
+                    }
+                    if (Connection.State == ConnectionState.Open) Connection.Close();
+                }
+                return Lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
