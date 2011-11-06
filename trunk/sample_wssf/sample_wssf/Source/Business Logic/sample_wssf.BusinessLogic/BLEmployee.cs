29a5cd97d167151852d12dc70e7f3b86716026a8
puentesarrin@gmail.com <puentesarrin@gmail.com@53c182f2-14e1-a3c1-e592-f3c75dab8e0f>
2011-11-06 00:29:55 +0000

    Initial commit

---
+++ trunk/sample_wssf/sample_wssf/Source/Business Logic/sample_wssf.BusinessLogic/BLEmployee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using sample_wssf.DataContracts;
+using sample_wssf.DataAccess;
+
+namespace sample_wssf.BusinessLogic
+{
+    public class BLEmployee
+    {
+        public ListDCEmployee SelectAllEmployees()
+        {
+            try
+            {
+                return new DAEmployee().SelectAllEmployees();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ trunk/sample_wssf/sample_wssf/Source/Service Interface/sample_wssf.ServiceImplementation/PartialClasses/SIPCServiceEmployee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using sample_wssf.MessageContracts;
+using sample_wssf.DataContracts;
+using sample_wssf.BusinessLogic;
+
+namespace sample_wssf.ServiceImplementation
+{
+    public partial class ServiceEmployee
+    {
+        public override sample_wssf.MessageContracts.selectAllEmployeesResponse SelectAllEmployees()
+        {
+            return new selectAllEmployeesResponse(){
+                ListEmployees = new BLEmployee().SelectAllEmployees()
+            };
+        }
+    }
+}
